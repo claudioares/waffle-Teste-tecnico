d5171fe61a7259df8aa766604e18d72fb44240ee
claudiojas <claudio.soares@lacreisaude.com.br>
2025-02-23 14:09:56 -0300

    add. graphics

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,6 +5,16 @@ import { IUserResponse } from "../services/interface";
 import { formatToBrazilianTime } from "../utils/formatToBrazilTime";
 import useEmailStorage from "../hooks/useEmailStorage";
 import { useNavigate } from 'react-router-dom';
+import {  
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent, } from "../components/ui/chart";
+import {CartesianGrid, Line, LineChart, XAxis} from "recharts";
+import { TrendingUp } from "lucide-react"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
+
+
 
 
 
@@ -24,12 +34,76 @@ export const Dashboard = () => {
     handleUserDataREpositorie();
   }, []);
 
+
   const handleLogout = () =>{
     removeEmail();
     navigate('/login');
   }
+
+  const chartData = [
+    { month: "January", desktop: 186 },
+    { month: "February", desktop: 305 },
+    { month: "March", desktop: 237 },
+    { month: "April", desktop: 73 },
+    { month: "May", desktop: 209 },
+    { month: "June", desktop: 214 },
+  ]
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "#6666",
+    },
+  } satisfies ChartConfig
   
   return (
+   <>
+    <Card className="w-2xl">
+      <CardHeader>
+        <CardTitle>Line Chart - Linear</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={true}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Line
+              dataKey="desktop"
+              type="linear"
+              stroke="var(--color-desktop)"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
     <div className="p-6 bg-gray-100 min-h-screen">
       {/* Cabeçalho */}
       <div className="flex justify-between items-center mb-8">
@@ -68,6 +142,7 @@ export const Dashboard = () => {
         </ul>
       </div>
     </div>
+   </>
   );
 };
 
